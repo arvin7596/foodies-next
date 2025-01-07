aa23c47b9773173b68dd6a43c172e9f3b97f8fee
Arvin Ghaffari <arvinghaffari75@gmail.com>
2025-01-07 23:49:35 +0100

    refactor : disable submit button and refresh data

--- components/meals/meals-form-submit.js
+++ components/meals/meals-form-submit.js
@@ -1,12 +1,14 @@
-'use client'
+"use client";
 
-import { useFormStatus } from 'react-dom'
+import { useFormStatus } from "react-dom";
 
 function MealsFormSubmit() {
-    const { pending } = useFormStatus()
-    return (
-        <button type="submit" disabled={pending}>{pending ? "Submitting... " : "Share Meal"}</button>
-    )
+  const { pending } = useFormStatus();
+  return (
+    <button type="submit" disabled={true}>
+      {pending ? "Submitting... " : "Share Meal"}
+    </button>
+  );
 }
 
-export default MealsFormSubmit
+export default MealsFormSubmit;
